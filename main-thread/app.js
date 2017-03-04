e522b1dd29b0ad799114496363bd33a798847eab
Martin Splitt <mr.avgp@googlemail.com>
2017-03-04 11:46:13 +0100

    Update app.js

--- main-thread/app.js
+++ main-thread/app.js
@@ -21,7 +21,7 @@ function runTest() {
 }
 
 function isPrime(candidate) {
-  for(var n=2; n < Math.floor(Math.sqrt(candidate)); n++) {
+  for(var n=2; n <= Math.floor(Math.sqrt(candidate)); n++) {
     if(candidate % n === 0) return false
   }
   return true
